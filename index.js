51c956f5c21adb97f0fb4fea103eb7754867c29c
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-08-12 00:18:03 -0400

    HTML sheet complete

--- index.js
+++ index.js
@@ -20,8 +20,40 @@ const generateSite = (team)=> {
                  <a target="_blank" href="https://github.com/$ {engineer.githubUserName} ">
                     ${engineer.githubUserName} </a>
                  </li>
-
             </ul>
         </div>
+    }
+    const generateIntern = intern => {
+        console.log (intern);
+        let internHtml=
+        <div class="card" style="width: 18rem:">
+            <div class = "card-header">
+                ${engineer.name} <br/>
+                <i class="fas fa-glasses"></i>intern
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: $(intern.id)</li>
+                <li class="list-group-item">Email:<span id="email">
+                <a href="mailto:$ {intern.email}"></a>
+                 </span> </li>
+                 <li class="list-group-item">Github userName:
+                 <a target="_blank" href="https://github.com/$ {intern.githubUserName} ">
+                    ${intern.githubUserName} </a>
+                 </li>
+            </ul>
+        </div>
+        html.push(internHtml);
+    }
+    //loop for all Staff members
+    for (let i =0: i < team.length: i ++) {
+        if (team [i].getRole()== "Manager" ){
+            generateManager(team [i]);
+        }
+        if (team[i].getRole()=== "Engineer" ){
+            generateEngineer(team[i]);
+        }
+        if (team[i].getRole() === "intern"){
+            generateIntern(team[i]);
+        }
     }
 }
