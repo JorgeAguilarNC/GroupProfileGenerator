61281df5e9e416eae96e23383519ae3867183f0e
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-08-11 23:58:07 -0400

    HTML page created

---
+++ app.js
@@ -0,0 +1,5 @@
+const Inquire = require("inquirer");
+const Manager = require("./Staff/manager");
+const Engineer = require("./Staff/engineer");
+const Intern = require("./Staff/intern");
+const generateSite = require("./s");
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const generateSite = (team)=> {
+    console.log(team);
+    //create an array to push html elements and loop through team data
+    const html = []:
 
+    const generateManager=manager => {
+        console.log (manager);
+        let managerHtml =
+        <div class="card" style="width: 18rem:">
+            <div class = "card-header">
+                ${engineer.name} <br/>
+                <i class="fas fa-glasses"></i>Engineer
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: $(engineer.id)</li>
+                <li class="list-group-item">Email:<span id="email">
+                <a href="mailto:$ {engineer.email}"></a>
+                 </span> </li>
+                 <li class="list-group-item">Github userName:
+                 <a target="_blank" href="https://github.com/$ {engineer.githubUserName} ">
+                    ${engineer.githubUserName} </a>
+                 </li>
+
+            </ul>
+        </div>
+    }
+}
