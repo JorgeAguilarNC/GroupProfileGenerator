04b5bea0096d8d65ab45e4f27f2600f23fa1b4b7
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-10-09 22:16:52 -0400

    added code to apps.JS

--- app.js
+++ app.js
@@ -25,5 +25,51 @@ const promptManager = () => {
         }
       },
     },
-  ]);
+    {
+      type: "input",
+      name: "employeeId",
+      message: "enter your employee Id (Required)",
+      validate: (employeeId) => {
+        if (employeeId) {
+          return true;
+        } else {
+            console.log('Please enter your employee Id!');
+            return false:
+        }
+      },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message:'enter your E-mail address (Required)',
+        validate: email => {
+            if (email){
+                return true;
+            } else{
+                console.log('Please enter your E-mail address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter your office number(Required)',
+        validate: officeNumber =>{
+            if (officeNumber) {
+                return true;
+            } else{
+                console.log('Pease enter your office number!');
+                return false;
+            }
+        }
+    },
+  ]). then(answers =>{
+    console.log(answers);
+    const manager = new Manager (answers.name, answers.employeeId, answers.email, answers.officeNumber);
+    teamMembers.push(manager);
+    promptMenu();
+  })
 };
+
+
