215bbab60f14370bec41596f6ede8277a5f86646
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-10-09 21:55:25 -0400

    added Apps JS and generate site JS

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-const Inquire = require("inquirer");
+const inquire = require("inquirer");
 const Manager = require("./Staff/manager");
 const Engineer = require("./Staff/engineer");
 const Intern = require("./Staff/intern");
-const generateSite = require("./s");
+const generateSite = require("../GroupProfileGenerator/generateSite");
+const fs = require("fs");
+const path = require("path");
+const { default: inquirer } = require("inquirer");
+const output_Dir = path.resolve(__dirname, "output");
+const outputPath = path.join(output_Dir, "team.html");
+const teamMembers = [];
+
+const promptManager = () => {
+  return inquire.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "what is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+  ]);
+};
