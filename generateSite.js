a21522e49d8baae47834bdf2f8ef1e72145d9f47
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-10-15 11:29:24 -0400

    errors fixed for test, generateSite and index

--- generateSite.js
+++ generateSite.js
@@ -1,88 +1,101 @@
 const generateTeam = (team) => {
-    console.log(team);
-    const html = [];
-    //generate manager card
-    const generateManager = manager => {
-      console.log(manager);
-      let managerHtml = (
-          //Bootstrap code
-        <div class='card' style='width: 18rem;'>
-          <div class='card-header'>
-            ${manager.name} <br />
-            <i class='fas fa-mug-hot'></i>Manager
-          </div>
-          <ul class='list-group list-group-flush'>
-            <li class='list-group-item'>ID: ${manager.id}</li>
-            <li class='list-group-item'>
-              Email:
-              <span id='email'>
-                <a href='mailto:${manager.email}'></a>{" "}
-              </span>
-            </li>
-            <li class='list-group-item'>office number:${manager.officeNumber}</li>
-          </ul>
+  console.log(team);
+  const html = [];
+  //generate manager card
+  const generateManager = (manager) => {
+    console.log(manager);
+    let managerHtml = (
+      //Bootstrap code
+      <div class='card' style='width: 18rem;'>
+        <div class='card-header'>
+          ${manager.name} <br />
+          <i class='fas fa-mug-hot'></i>Manager
         </div>
-      );
-      html.push(managerHtml):
-    }
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>ID: ${manager.id}</li>
+          <li class='list-group-item'>
+            Email:
+            <span id='email'>
+              <a href='mailto:${manager.email}'></a>{" "}
+            </span>
+          </li>
+          <li class='list-group-item'>office number:${manager.officeNumber}</li>
+        </ul>
+      </div>
+    );
+    html.push(managerHtml);
+  };
 
-    const generateEngineer = engineer => {
-      console.log(engineer);
-      let engineerHtml = (
-        <div class='card' style='width: 18rem;'>
-          <div class='card-header'>
-            ${engineer.name} <br />
-            <i class='fas fa-mug-hot'></i>engineer
-          </div>
-          <ul class='list-group list-group-flush'>
-            <li class='list-group-item'>ID: ${engineer.id}</li>
-            <li class='list-group-item'>
-              Email:
-              <span id='email'>
-                <a href='mailto:${engineer.email}'>${engineer.email}</a>
-              </span>
-            </li>
-            <li class='list-group-item'>Github username: <a target="blank" href="https://github.com/${engineer.githubUsername}">${engineer.githubUsername}</a>  </li>
-          </ul>
+  const generateEngineer = (engineer) => {
+    console.log(engineer);
+    let engineerHtml = (
+      <div class='card' style='width: 18rem;'>
+        <div class='card-header'>
+          ${engineer.name} <br />
+          <i class='fas fa-mug-hot'></i>engineer
         </div>
-      );
-      html.push(engineerHtmlHTML);
-    }
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>ID: ${engineer.id}</li>
+          <li class='list-group-item'>
+            Email:
+            <span id='email'>
+              <a href='mailto:${engineer.email}'>${engineer.email}</a>
+            </span>
+          </li>
+          <li class='list-group-item'>
+            Github username:{" "}
+            <a
+              target='blank'
+              href='https://github.com/${engineer.githubUsername}'>
+              ${engineer.githubUsername}
+            </a>{" "}
+          </li>
+        </ul>
+      </div>
+    );
+    html.push(engineerHtmlHTML);
+  };
 
-    const generateIntern = intern => {
-        console.log(intern);
-        let internHtml = (
-          <div class='card' style='width: 18rem;'>
-            <div class='card-header'>
-              ${intern.name} <br />
-              <i class='fas fa-user-graduate'></i>intern
-            </div>
-            <ul class='list-group list-group-flush'>
-              <li class='list-group-item'>ID: ${intern.id}</li>
-              <li class='list-group-item'> Email: <span id="email"><a href="mailto: ${intern.email}">${intern.email}</a> </span> </li>
-              <li class='list-group-item'>school: ${intern.school}  </li>
-            </ul>
-          </div>
-        );
-        html.push(internHtmlHTML);
-      }
-      //for look for employees
-      for(let i = 0; i <team.length; i++) {
-        if (team[i].getrole()==="manager"){
-            generateManager(team[i]);
-        }
-        if (team[i].getrole() === "engineer"){
-            generateIntern(team[i]);
-        }
-        if (team[i].getrole() === "intern"){
-            generateIntern(team[i]);
-        }
-      }
-      //html blocks
-      return html.join('');
-}
-module.exports = team =>{
-    return `
+  const generateIntern = (intern) => {
+    console.log(intern);
+    let internHtml = (
+      <div class='card' style='width: 18rem;'>
+        <div class='card-header'>
+          ${intern.name} <br />
+          <i class='fas fa-user-graduate'></i>intern
+        </div>
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>ID: ${intern.id}</li>
+          <li class='list-group-item'>
+            {" "}
+            Email:{" "}
+            <span id='email'>
+              <a href='mailto: ${intern.email}'>${intern.email}</a>{" "}
+            </span>{" "}
+          </li>
+          <li class='list-group-item'>school: ${intern.school} </li>
+        </ul>
+      </div>
+    );
+    html.push(internHtmlHTML);
+  };
+  //for look for employees
+  for (let i = 0; i < team.length; i++) {
+    if (team[i].getrole() === "manager") {
+      generateManager(team[i]);
+    }
+    if (team[i].getrole() === "engineer") {
+      generateIntern(team[i]);
+    }
+    if (team[i].getrole() === "intern") {
+      generateIntern(team[i]);
+    }
+  }
+  //html blocks
+  return html.join("");
+};
+module.exports = (team) => {
+  return `
 <!DOCTYPE html>
 <html lang="en">
 
@@ -105,6 +118,4 @@ module.exports = team =>{
 </body>
 </html>
 `;
-}
-
-
+};
--- index.js
+++ index.js
@@ -1,59 +1,75 @@
-const generateSite = (team)=> {
-    console.log(team);
-    //create an array to push html elements and loop through team data
-    const html = []:
+const generateSite = (team) => {
+  console.log(team);
+  //create an array to push html elements and loop through team data
+  const html = [];
 
-    const generateManager=manager => {
-        console.log (manager);
-        let managerHtml =
-        <div class="card" style="width: 18rem:">
-            <div class = "card-header">
-                ${engineer.name} <br/>
-                <i class="fas fa-glasses"></i>Engineer
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: $(engineer.id)</li>
-                <li class="list-group-item">Email:<span id="email">
-                <a href="mailto:$ {engineer.email}"></a>
-                 </span> </li>
-                 <li class="list-group-item">Github userName:
-                 <a target="_blank" href="https://github.com/$ {engineer.githubUserName} ">
-                    ${engineer.githubUserName} </a>
-                 </li>
-            </ul>
+  const generateManager = (manager) => {
+    console.log(manager);
+    let managerHtml = (
+      <div class='card' style='width: 18rem:'>
+        <div class='card-header'>
+          ${engineer.name} <br />
+          <i class='fas fa-glasses'></i>Engineer
         </div>
-    }
-    const generateIntern = intern => {
-        console.log (intern);
-        let internHtml=
-        <div class="card" style="width: 18rem:">
-            <div class = "card-header">
-                ${engineer.name} <br/>
-                <i class="fas fa-glasses"></i>intern
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: $(intern.id)</li>
-                <li class="list-group-item">Email:<span id="email">
-                <a href="mailto:$ {intern.email}"></a>
-                 </span> </li>
-                 <li class="list-group-item">Github userName:
-                 <a target="_blank" href="https://github.com/$ {intern.githubUserName} ">
-                    ${intern.githubUserName} </a>
-                 </li>
-            </ul>
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>ID: $(engineer.id)</li>
+          <li class='list-group-item'>
+            Email:
+            <span id='email'>
+              <a href='mailto:$ {engineer.email}'></a>
+            </span>{" "}
+          </li>
+          <li class='list-group-item'>
+            Github userName:
+            <a
+              target='_blank'
+              href='https://github.com/$ {engineer.githubUserName} '>
+              ${engineer.githubUserName}{" "}
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  const generateIntern = (intern) => {
+    console.log(intern);
+    let internHtml = (
+      <div class='card' style='width: 18rem:'>
+        <div class='card-header'>
+          ${engineer.name} <br />
+          <i class='fas fa-glasses'></i>intern
         </div>
-        html.push(internHtml);
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>ID: $(intern.id)</li>
+          <li class='list-group-item'>
+            Email:
+            <span id='email'>
+              <a href='mailto:$ {intern.email}'></a>
+            </span>{" "}
+          </li>
+          <li class='list-group-item'>
+            Github userName:
+            <a
+              target='_blank'
+              href='https://github.com/$ {intern.githubUserName} '>
+              ${intern.githubUserName}{" "}
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+    html.push(internHtml);
+  };
+  //loop for all Staff members
+  for (let i = 0; i < team.length; i++) {
+    if (team[i].getRole() == "Manager") {
+      generateManager(team[i]);
     }
-    //loop for all Staff members
-    for (let i =0: i < team.length: i ++) {
-        if (team [i].getRole()== "Manager" ){
-            generateManager(team [i]);
-        }
-        if (team[i].getRole()=== "Engineer" ){
-            generateEngineer(team[i]);
-        }
-        if (team[i].getRole() === "intern"){
-            generateIntern(team[i]);
-        }
+    if (team[i].getRole() === "Engineer") {
+      generateEngineer(team[i]);
     }
-}
+    if (team[i].getRole() === "intern") {
+      generateIntern(team[i]);
+    }
+  }
+};
